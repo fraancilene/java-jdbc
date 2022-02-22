bb8217e65e4090d08cb2e5715af102d3a80824ae
Francilene Silva <francilenesilva.fps10@gmail.com>
2022-02-22 16:43:39 +0000

    recuperando dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,36 @@ package application;
 
 import db.DB;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
-    public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+    public static void main(String[] args) throws SQLException {
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from tb_department");
+
+            // percorrendo os dados e capturando
+            while (rs.next()){
+                System.out.println(rs.getInt("id") + " - " + rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            // fechando os recursos
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
 
     }
 
--- src/db/DB.java
+++ src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -14,7 +12,7 @@ public class DB {
 
 	// método para conectar ao banco de dados - conectar com o banco no jdbc é instanciar um objeto do tipo Connection
 	public static Connection getConnection() {
-		if (conn == null){
+		if (conn == null) {
 			try {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
@@ -27,7 +25,7 @@ public class DB {
 	}
 
 	// metodo para fechar a conexão com o banco de dados
-	public static void closeConnection(){
+	public static void closeConnection() {
 		if (conn != null) {
 			try {
 				conn.close();
@@ -39,12 +37,33 @@ public class DB {
 
 	// buscando os dados do arquivo db.properties para conectar ao banco de dados
 	private static Properties loadProperties() {
-		try(FileInputStream fs = new FileInputStream("db.properties")){
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
 		} catch (IOException e) {
 			throw new DbException(e.getMessage());
+		}
+	}
+
+	// metodos para fechar os objetos
+	public static void closeStatement(Statement st){
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs){
+		if (rs != null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
 		}
 	}
 }
